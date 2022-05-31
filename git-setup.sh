f6048e93c4842f1cfcec475ae151c2cce5246466
wisnoskij <jonathonwisnoski@gmail.com>
2022-05-30 22:17:53 -0400

    default to merge

--- git-setup.sh
+++ git-setup.sh
@@ -6,4 +6,5 @@ sudo dnf install git
 
 git config --global user.name "wisnoskij"
 git config --global user.email "jonathonwisnoski@gmail.com"
-git config --global core.editor vi
+git config --global core.editor vii
+git config --global pull.rebase false
