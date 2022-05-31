73d6cb922418153b836f37ccaa6e78894303509b
wisnoskij <jonathonwisnoski@gmail.com>
2022-05-30 21:38:39 -0400

    made main bootstrap file work better in more situations.

--- fedora-fromscratch.sh
+++ fedora-fromscratch.sh
@@ -2,14 +2,26 @@
 
 #minimal Networking, standard, (guest, but dont install that on metal)
 name="fedora-fromscratch"
+prjpath="$HOME/$name"
+function gitname {
+	$(basename -s .git `git config --get remote.origin.url`
+}
 
 sudo dnf update
+sudo dnf install git
 
-if [ ! -f "$name.sh" ]; then
-	cd ~
-	sudo dnf install git
-	git clone "https://github.com/wisnoskij/$name.git"
-	cd $name
+if [ $name == gitname ]; then
+	git pull
+elif [ -d $prjpath ]; then
+	cd $prjpath
+	if [ $name == gitname ]; then
+        	git pull
+	else
+		echo "ERROR: $prjpath already exists. Remove and run again."
+	fi
+else
+	git clone "https://github.com/wisnoskij/$name.git" $prjpath
+	cd $prjpath
 fi
 
 "./$name.sh"
