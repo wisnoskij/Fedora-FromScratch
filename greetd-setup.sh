b902186a0bfde3f5aff8ac5b1b42fdaeebef416f
wisnoskij <jonathonwisnoski@gmail.com>
2022-05-31 23:42:01 -0400

    cleanup, commenting

--- greetd-setup.sh
+++ greetd-setup.sh
@@ -6,9 +6,11 @@ sudo dnf install greetd
 sudo systemctl enable greetd
 sudo systemctl set-default graphical.target
 
+#$SHELL is not even set yet, so login cannot run correctly with this cmd set
+sudo sed -i 's|--cmd $SHELL|--cmd /bin/bash|' /etc/greetd/config.toml
+
 ########## Greeter (tuigreet) ##########
 sudo dnf copr enable alebastr/sway-extras
 sudo dnf install greetd-tuigreet
 
-sudo sed -i 's/agreety/tuigreet/' /etc/greetd/config.toml		#Run tuigreet as the greeter
-sudo sed -i 's|--cmd $SHELL|--cmd /bin/bash|' /etc/greetd/config.toml	#$SHELL is not even set yet, so login cannot run correctly with this cmd set
+sudo sed -i 's/agreety/tuigreet/' /etc/greetd/config.toml	#Run tuigreet as the greeter
