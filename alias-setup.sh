214baa4f1d44a9cd5cdeabd46cbce3d23d30889e
wisnoskij <jonathonwisnoski@gmail.com>
2022-06-02 19:36:30 -0400

    finished alias setup

--- alias-setup.sh
+++ alias-setup.sh
@@ -3,15 +3,13 @@
 ########## Alias Setup ##########
 
 ALIAS_PATH="$HOME/.bashrc.d"
-GIT_ALIAS="$ALIAS_PATH/git-alias.sh"
+FS_ALIAS="fromscratch-alias.bashrc"
 
 #Fedora's bashrc uses .bashrc.d folder for any additions
 if [ ! -d $ALIAS_PATH ]; then
 	mkdir $ALIAS_PATH
+	chmod 711 $ALIAS_PATH
 fi
 
-#Create file if needed and set to xrw,xr,xr i guess
-if [ ! -d "$GIT_ALIAS" ]; then
-else
-	echo "$GIT_ALIAS already exists. Overwrite? [y/N]:"
-fi
+chmod 711 $FS_ALIAS
+mv $FS_ALIAS "$ALIAS_PATH/$FS_ALIAS"
